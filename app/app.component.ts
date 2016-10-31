9f5a49707b43083ceaec6aa577fcd865763aea7a
Jorge Sepulveda <jorluiseptor@gmail.com>
2016-10-31 11:36:49 -0400

    About to begin Dashboard Top Heroes section.

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,8 +2,13 @@ import { Component } from '@angular/core';
 
 @Component({
     selector:'my-app',
-    template:`<h1>{{title}}</h1>
-              <my-heroes></my-heroes>
+    template:`
+                <h1>{{title}}</h1>
+                <nav>
+                    <a routerLink="/dashboard">Dashboard</a>
+                    <a routerLink="/heroes">Heroes</a>
+                </nav>
+                <router-outlet></router-outlet>
              `
 })
 
